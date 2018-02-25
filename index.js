bc545339722f1e277a8328cb76f97c8f22632da5
Ewan <tech-a06@tech-a06s-air.home>
2018-02-25 14:00:35 +0000

    Added and configured mongoose

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const ejs = require("ejs");
 const fs = require("fs");
 const express = require("express");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 const app = express();
 app.set("view engine", "ejs");
 const port = 3000;
@@ -8,22 +10,46 @@ const mongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/meta-social";
 let posts;
 
-function connectToDB(dbUrl) {
-	mongoClient.connect(dbUrl, function(err, db) {
-	  if (err) console.log(err);
-	  console.log("Database created!");
-	  var dbo = db.db("mydb");
-	  dbo.createCollection("customers", function(err, res) {
-	    if (err) console.log(err);
-	    console.log("Collection created!");
-	    db.close();
-	  });
-	});
+async function databaseSetup() {
+	//Check for the DB_HOST environment variable
+	if (process.env.DB_HOST) {
+		console.log(`Waiting for connection from ${process.env.DB_HOST}`);
+		//Connect to the DB if the env variable is there
+		const connection = await mongoose.createConnection(process.env.DB_HOST + "/lab-db").catch((err) => {
+			if (err) {
+				//Could not establish a connection
+				console.log(`Could not connect to ${process.env.DB_HOST}, please check database IP address`);
+				console.log("Then run 'export DB_HOST=mongodb://[database ip]'");
+				console.log("Exiting...");
+				process.exit();
+			}
+		});
+		//Connection established
+		//Set the schema of the database
+		const schema = new Schema({
+			userFirstName: String,
+			userLastName: String
+		});
+		//Set up the model for communicating with the database
+		Result = connection.model('Result', schema);
+		console.log(`Connected sucessfully to ${process.env.DB_HOST}/meta-social-db`);
+		//Now start the server
+		console.log("Starting server...");
+		startServer();
+	} else {
+		//User has no DB_HOST environment variable set for the db ip
+		console.log("Oh no... no DB_HOST environment variable set. Cannot connect to DB.");
+		console.log("Please run 'export DB_HOST=mongodb://[database ip]'");
+		console.log("Exiting...");
+		process.exit();
+	}
 }
 
 function startServer() {
 	app.get("/", (req, res) => {
-		res.render("index", {
+		Result.find((err, results) => {
+			if (err) return console.log(err);
+			res.render("index", {results: results});
 		});
 	});
 	app.get("/global.css", (req, res) => {
@@ -43,5 +69,4 @@ function startServer() {
 	});
 }
 
-connectToDB(url);
-startServer();
+databaseSetup();
